034deb7e294b4166ddeca8c8fad32cda0b60185d
@trey.rosales_bespin <trey_philip.rosales@us.af.mil>
2026-03-23 14:06:52 -0500

    everything

--- BasketballShotSim/Assets/BasketballGame2D.cs
+++ BasketballShotSim/Assets/BasketballGame2D.cs
@@ -13,14 +13,10 @@ public class BasketballGame2D : MonoBehaviour
     private float power = 6f;
     private float angle = 70f;
 
-    private float hoopHeight;
-    private float hoopDistance;
-
     private int score = 0;
 
     void Start()
     {
-        ResetHoop();
         ResetBall();
         UpdateScoreUI();
     }
@@ -38,23 +34,22 @@ public class BasketballGame2D : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.R))
         {
             ResetBall();
-            ResetHoop();
         }
     }
 
     void HandleInput()
     {
         if (Input.GetKey(KeyCode.UpArrow))
-            angle += 40f * Time.deltaTime;
+            angle += 1f * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.DownArrow))
-            angle -= 40f * Time.deltaTime;
+            angle -= 1f * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.RightArrow))
-            power += 5f * Time.deltaTime;
+            power += 1f * Time.deltaTime;
 
         if (Input.GetKey(KeyCode.LeftArrow))
-            power -= 5f * Time.deltaTime;
+            power -= 1f * Time.deltaTime;
 
         power = Mathf.Clamp(power, 1f, 10f);
         angle = Mathf.Clamp(angle, 0f, 90f);
@@ -67,9 +62,10 @@ public class BasketballGame2D : MonoBehaviour
         Vector2 force = new Vector2(
             Mathf.Cos(radians),
             Mathf.Sin(radians)
-        ) * power *2f;
+        ) * power * 2f;
 
         rb.linearVelocity = Vector2.zero;
+
         transform.position = spawnPoint.position;
 
         rb.AddForce(force, ForceMode2D.Impulse);
@@ -86,15 +82,6 @@ public class BasketballGame2D : MonoBehaviour
     void ResetAfterShot()
     {
         ResetBall();
-        ResetHoop();
-    }
-
-    void ResetHoop()
-    {
-        hoopHeight = Random.Range(2f, 4f);
-        hoopDistance = Random.Range(4f, 8f);
-
-        hoop.position = new Vector3(hoopDistance, hoopHeight, 0);
     }
 
     void ResetBall()
